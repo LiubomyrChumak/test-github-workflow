6ac6cae92c79d1790aa5699573251178c7aa068e
Liubomyr Chumak <63431742+LiubomyrChumak@users.noreply.github.com>
2023-05-25 19:38:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 
-
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-  return '<h1>Hello WSB! Greetings from Flask!</h1>'
+    """
+    Handle the root endpoint.
+    """
+    return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
